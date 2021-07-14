0ddf2dcd6adebf885cd34edc866217a494c03887
Douglas Figueiredo <douglasarts@gmail.com>
2021-07-14 16:25:34 -0300

    Fix remove styled in App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import * as S from "./styled";
 import Header from "./components/Header";
 import Cadastro from "./components/Cadastro";
 
 function App() {
   return (
-    <S.Conainer>
+    <>
       <Header />
       <Cadastro />
-    </S.Conainer>
+    </>
   );
 }
 
